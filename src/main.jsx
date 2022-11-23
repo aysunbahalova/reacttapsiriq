10025f4ed4bdf99876483dfe6b425710ff53a704
aysunbahalova <114071754+aysunbahalova@users.noreply.github.com>
2022-11-23 02:44:42 -0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Button from './components/Web4/Web'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <Button/>
+    </div>
+)
